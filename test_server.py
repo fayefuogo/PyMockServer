cd159f21fdb3d75fb5acf99cfe0ac7b0ad81a03d
fayefuogo <fayeiwueze@gmail.com>
2025-08-11 15:37:44 -0400

    update test_server.py

--- test_server.py
+++ test_server.py
@@ -2,13 +2,9 @@ import subprocess
 import time
 import socket
 
-# Start the server in a separate process
 server_process = subprocess.Popen(["python", "server.py"])
-
-# Give the server some time to start
 time.sleep(10)
 
-# Then run the tests (e.g., test server responses)
 def test_server_get_response():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(("127.0.0.1", 8080))
@@ -25,5 +21,4 @@ def test_invalid_request():
     assert "400 Bad Request" in response
     client_socket.close()
 
-# Terminate the server after tests
 server_process.terminate()
